65212eb22f51a9e442cb7c057176ea038f5628c3
hp001 <hp001@DESKTOP-KVQ7G5C>
2024-11-19 11:22:25 +0530

    first push

---
+++ Cars/src/test/java/Automotors/RollsRoyceTest.java
@@ -0,0 +1,17 @@
+package Automotors;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class RollsRoyceTest {
+	@Test
+	public void rollsRoyce() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.rolls-roycemotorcars.com/en_GB/home.html");
+	}
+}
